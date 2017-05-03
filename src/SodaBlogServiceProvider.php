877e7ae697d7eaabace0d353519acaf9fbbca7e5
ryzr <ryzaar@hotmail.com>
2017-05-03 02:47:24 +0000

    Apply fixes from StyleCI

--- src/SodaBlogServiceProvider.php
+++ src/SodaBlogServiceProvider.php
@@ -55,7 +55,7 @@ class SodaBlogServiceProvider extends ServiceProvider
 
                     $menu->addItem('Blog', [
                         'url'         => route('soda.cms.blog.index'),
-                        'label'       => ucfirst(trans('soda-blog::general.blog')) . ' ' . ucfirst(trans('soda-blog::general.posts')),
+                        'label'       => ucfirst(trans('soda-blog::general.blog')).' '.ucfirst(trans('soda-blog::general.posts')),
                         'icon'      => 'fa fa-book',
                         'isCurrent'   => soda_request_is(trim($blog_cms_slug, '/').'*') && ! soda_request_is(trim($blog_cms_slug, '/').'/settings*') && ! soda_request_is(trim($blog_cms_slug, '/').'/import*'),
                         'permissions' => 'manage-blog',
@@ -63,7 +63,6 @@ class SodaBlogServiceProvider extends ServiceProvider
                 });
             }
         }
-
 
         app('soda.drafting')->registerDraftables([Post::class]);
 
